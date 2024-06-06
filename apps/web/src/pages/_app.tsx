8faa3810fb704dc58186bc99a655fcf4aa5f233e
okamonster <okamonster0615@gmail.com>
2024-06-07 04:34:49 +0900

    feat:add create project

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,8 +1,8 @@
-/* eslint-disable @typescript-eslint/naming-convention */
 import type { AppProps } from 'next/app'
 
 import '~/styles/globals.css'
 
+// eslint-disable-next-line @typescript-eslint/naming-convention
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
